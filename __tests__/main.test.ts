104baaad727033712e0c5a86eadaabc6649ce047
per1234 <accounts@perglass.com>
2021-05-03 20:39:28 -0700

    Remove extraneous text from license file
    In order to be recognized by GitHub's automatic license detection system, the license file must contain only the exact
    license text, which can be sourced from choosealicense.com. Please do not modify this file in any way.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,15 @@
+// Copyright (c) 2019 ARDUINO SA
+//
+// The software is released under the GNU General Public License, which covers the main body
+// of the arduino/setup-taskfile code. The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to modify or
+// otherwise use the software for commercial activities involving the Arduino
+// software without disclosing the source code of your own applications. To purchase
+// a commercial license, send an email to license@arduino.cc
+
 import io = require("@actions/io");
 import path = require("path");
 import os = require("os");
